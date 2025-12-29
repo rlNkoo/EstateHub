5faa88210750c741e85eef4554129465b0616d06
rlNkoo <rolakartur96@gmail.com>
2025-12-29 10:26:28 +0100

    Create project template

---
+++ notification-service/src/test/java/com/rlnkoo/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlnkoo.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
