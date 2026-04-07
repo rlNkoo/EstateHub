1eab6eb993c18a27cf8e62a21db7b11ba4df936b
rlNkoo <rolakartur96@gmail.com>
2026-04-07 19:27:45 +0200

    Add Integration test

--- notification-service/src/test/java/com/rlnkoo/notificationservice/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/rlnkoo/notificationservice/NotificationServiceApplicationTests.java
@@ -2,12 +2,13 @@ package com.rlnkoo.notificationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class NotificationServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
